7eca224241858e2e62b33150543ecaf970c8c099
MariamKONE <mariam.kone@lepoles.com>
2017-07-25 16:03:35 +0100

    commit56
    commit56

--- front/index.php
+++ front/index.php
@@ -19,7 +19,7 @@ require_once("inc/init.inc.php");
 
     <title>Developpeur web freelance paris</title>
 
-    <link rel="stylesheet" href="../css/style.css">
+
 
     <!-- Bootstrap Core CSS -->
     <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
@@ -35,6 +35,8 @@ require_once("inc/init.inc.php");
     <!-- Theme CSS -->
     <link href="css/creative.min.css" rel="stylesheet">
 
+    <link rel="stylesheet" href="../css/style.css">
+
     <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
     <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
     <!--[if lt IE 9]>
@@ -43,105 +45,179 @@ require_once("inc/init.inc.php");
     <![endif]-->
 
 </head>
+<body >
 
-<body id="page-top">
-    <nav id="mainNav" class="navbar navbar-default navbar-fixed-top">
-        <div class="container-fluid">
-            <!-- Brand and toggle get grouped for better mobile display -->
-            <div class="navbar-header">
-                <!-- <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
-                    <span class="sr-only">Toggle navigation</span> Menu <i class="fa fa-bars"></i>
-                </button> -->
-                <a class="navbar-brand page-scroll" href="#page-top"><?php echo $ligne_utilisateur['prenom'].' '.$ligne_utilisateur['nom']; ?></a>
-            </div>
-
-            <!-- Collect the nav links, forms, and other content for toggling -->
-            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-
-                <ul class="nav navbar-nav navbar-right">
-
-                    <?php
-                        $sql = $pdoCV->query(" SELECT * FROM formations WHERE utilisateur_id ='1' ");
-                        $ligne_formation = $sql->fetch();
-                    ?>
-                    <li>
-                        <a class="page-scroll" href="#formation"><?php echo $ligne_formation['titre_f']; ?></a>
-                    </li>
-
-                    <?php
-                        $sql = $pdoCV->query(" SELECT * FROM experiences WHERE utilisateur_id ='1' ");
-                        $ligne_experience = $sql->fetch();
-                    ?>
-                    <li>
-                        <a class="page-scroll" href="#experience"><?php echo $ligne_experience['titre_e']; ?></a>
-                    </li>
-
-                    <?php
-                        $sql = $pdoCV->query(" SELECT * FROM competences WHERE utilisateur_id ='1' ");
-                        $ligne_competence = $sql->fetch();
-                    ?>
-                    <li>
-                        <a class="page-scroll" href="#competence"><?php echo $ligne_competence['titre_c']; ?></a>
-                    </li>
-
-                    <?php
-                        $sql = $pdoCV->query(" SELECT * FROM realisations WHERE utilisateur_id ='1' ");
-                        $ligne_realisation = $sql->fetch();
-                    ?>
-                    <li>
-                        <a class="page-scroll" href="#realisation"><?php echo $ligne_realisation['titre_r']; ?></a>
-                    </li>
-
-                    <?php
-                        $sql = $pdoCV->query(" SELECT * FROM atouts WHERE utilisateur_id ='1' ");
-                        $ligne_atout = $sql->fetch();
-                    ?>
-                    <li>
-                        <a class="page-scroll" href="#portfolio"><?php echo $ligne_atout['titre_a']; ?></a>
-                    </li>
-
-                    <?php
-                        $sql = $pdoCV->query(" SELECT * FROM interets WHERE utilisateur_id ='1' ");
-                        $ligne_interet = $sql->fetch();
-                    ?>
-                    <li>
-                        <a class="page-scroll" href="#portfolio"><?php echo $ligne_interet['titre_i']; ?></a>
-                    </li>
-
-                    <li>
-                        <a class="page-scroll" href="#contact">Contact</a>
-                    </li>
-
-                    <li>
-                        <a class="page-scroll" href="#telecharger">Telecharger CV</a>
-                    </li>
-                </ul>
-            </div>
-            <!-- /.navbar-collapse -->
+    <div id="preloader">
+        <div class="loader">
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
         </div>
-        <!-- /.container-fluid -->
-    </nav>
+    </div>
 
-    <header>
-        <div class="header-content">
-            <div class="header-content-inner">
-                <h1 id="homeHeading">
-                    <?php
-                    $sql = $pdoCV->query(" SELECT * FROM titres_cv WHERE utilisateur_id ='1' ");
-                    $ligne_titre = $sql->fetch();
-                ?><?php echo $ligne_titre['titre_cv']; ?></h1>
-                <hr>
-                <p><?php echo $ligne_titre['accroche']; ?></p>
+    <!--=========
+    3 ) Homepage
+    ==========-->
+        <div class="home-page">
 
+            <!--( a ) Introduction -->
+
+            <div class="introduction">
+                <img alt="" src="images/home_dp.jpg">
+                <div class="mask">
+                </div>
+                <div class="intro-content">
+                    <h1>HELLO<br>
+                    Je suis <span>Mariam</span> KONE</h1>
+                    <h2>Développeur web intégrateur</h2>
+                    <!-- Réseaux sociaux -->
+                    <p class="social-media hidden-xs">
+                        <a href="#" class="fa fa-facebook" data-toggle="tooltip" title="Facebook"></a>
+                        <a href="#" class="fa fa-twitter" data-toggle="tooltip" title="Twitter"></a>
+                        <a href="#" class="fa fa-plus" data-toggle="tooltip" title="Google+"></a>
+                        <a href="#" class="fa fa-linkedin" data-toggle="tooltip" title="Linkedin"></a>
+                        <a href="#" class="fa fa-behance" data-toggle="tooltip" title="Behance"></a>
+                        <a href="#" class="fa fa-flickr" data-toggle="tooltip" title="Flicker"></a>
+                        <a href="#" class="fa fa-instagram" data-toggle="tooltip" title="Instagram"></a>
+                    </p>
+                    <!-- Réseaux sociaux [ Fin ] -->
+
+                </div>
+            </div>
+
+            <!-- ( b ) Navigation Menu -->
+
+    <div class="menu">
+        <!-- Menu Boutton formation [ DEBUT ]  -->
+        <div class="profile-btn">
+            <img alt="" src="../front/img/portfolio/fullsize/accueil.jpg">
+            <div class="mask">
+            </div>
+            <div class="heading col-xs-11 col-xs-offset-1">
+                <div class="col-xs-2 hidden-xs">
+                    <i class="fa fa-user"></i>
+                </div>
+                <div class="col-sm-10">
+                    <h2>For<span>ma</span>tions</h2>
+                    <h3>Some words about me.</h3>
+                </div>
             </div>
         </div>
-    </header>
+        <!-- Menu Boutton formation [ FIN ]  -->
+
+        <div class="resume-btn">
+            <img alt="" src="">
+            <div class="mask">
+            </div>
+            <div class="heading col-xs-11 col-xs-offset-1">
+                <div class="col-xs-2 hidden-xs">
+                    <i class="fa fa-graduation-cap"></i>
+                </div>
+                <div class="col-sm-10">
+                    <h2>Ex<span>pé</span>riences</h2>
+                    <h3>Something about my experience and skills.</h3>
+                </div>
+            </div>
+        </div>
+        <!-- Menu Boutton Expérience [ FIN ]  -->
+
+        <div class="portfolio-btn">
+            <img alt="" src="">
+            <div class="mask">
+            </div>
+            <div class="heading col-xs-11 col-xs-offset-1">
+                <div class="col-xs-2 hidden-xs">
+                    <i class="fa fa-briefcase"></i>
+                </div>
+                <div class="col-sm-10">
+                    <h2>Réa<span>li</span>sation</h2>
+                    <h3>Have a spoon of my work.</h3>
+                </div>
+            </div>
+        </div>
+        <!-- Menu Boutton Réalisation [ Fin ]  -->
+
+
+
+        <!-- <div class="col-md-6"> -->
+
+            <!-- <div class="header-content" style=" height: 100vh; background-image:url(../front/img/portfolio/fullsize/accueil.jpg); background-repeat:no-repeat; background-size: 200% 100%; background-position: 75% center;"> -->
+                <div class="mask">
+                </div>
+
+                <div class="header-content-inner">
+                    <h1 id="homeHeading" style="text-align: center; margin:0;">Développeur Intégrateur web</h1>
+                    <br>
+                    <p>Titulaire d’un Master 2 en E-Business et communication des grandes marques, je me
+                    forme actuellement <span>aux métiers d’intégrateur et développeur web</span>. Indépendante et
+                    créative, je recherche un stage pour compléter ma formation.</p>
+
+                    <!-- Réseaux sociaux -->
+                    <p class="social-media hidden-xs">
+                        <a id="reseaux" href="#" class="fa fa-facebook" data-toggle="tooltip" title="Facebook"></a>
+                        <a id="reseaux" href="#" class="fa fa-twitter" data-toggle="tooltip" title="Twitter"></a>
+                        <a id="reseaux" href="#" class="fa fa-plus" data-toggle="tooltip" title="Google+"></a>
+                        <a id="reseaux" href="#" class="fa fa-linkedin" data-toggle="tooltip" title="Linkedin"></a>
+                        <a id="reseaux" href="#" class="fa fa-behance" data-toggle="tooltip" title="Behance"></a>
+                        <a id="reseaux" href="#" class="fa fa-flickr" data-toggle="tooltip" title="Flicker"></a>
+                        <a id="reseaux" href="#" class="fa fa-instagram" data-toggle="tooltip" title="Instagram"></a>
+                    </p>
+                </div>
+            </div>
+        </div>
+
+    <!-- Navigation menu -->
+        <div class="col-md-6">
+
+            <div class="menu">
+
+                <div class="formation" style=" background-color: red;">
+                    <img src="" alt="">
+                        <div class="mask">
+                                </div>
+                            <div class="heading col-xs-11 col-xs-offset-1">
+                                <div class="col-xs-2 hidden-xs">
+                                    <i class="fa fa-graduation-cap"></i>
+                                </div>
+                <div class="col-sm-10">
+                    <h2>Formations</h2>
+                    <h3>Some word about me</h3>
+                </div>
+            </div>
+
+
+                </div>
+
+                <div class="experience" style=" background-color: blue;">
+                    <img src="" alt="">
+                        <div class="mask">
+                                </div>
+                            <div class="heading col-xs-11 col-xs-offset-1">
+
+                            </div>
+                            <div class="col-xs-2 hidden-xs">
+                                <i class="fa fa-user"></i>
+                            </div>
+                <div class="col-sm-10">
+                    <h2>Expérience</h2>
+                    <h3>Some word about me</h3>
+                </div>
+                </div>
+
+
+
+    </div>
+    </div>
+
+
+    <!-- </header> -->
 
     <section class="bg-primary" id="formation">
         <div id="img_formation" class="container">
             <div class="row">
                 <div class="col-lg-8 col-lg-offset-2 text-center">
-                    <h2 class="section-heading">Formation</h2>
+                    <h2 class="section-heading">Formations</h2>
                     <hr class="light">
                     <p id="test" class="text-faded">Start Bootstrap has everything you need to get your new website up and running in no time! All of the templates and themes on Start Bootstrap are open source, free to download, and easy to use. No strings attached!</p>
 
@@ -154,11 +230,13 @@ require_once("inc/init.inc.php");
         <div class="container">
             <div class="row">
                 <div  class="col-lg-12 text-center">
-                    <h2 class="section-heading">Experience</h2>
+                    <h2 class="section-heading">Expériences</h2>
                     <hr class="primary">
                 </div>
             </div>
         </div>
+
+
         <!-- <div class="container">
             <div class="row">
                 <div class="col-lg-3 col-md-6 text-center">
@@ -192,98 +270,16 @@ require_once("inc/init.inc.php");
             </div>
         </div> -->
     </section>
+    <section class="bg-primary" id="competence">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-8 col-lg-offset-2 text-center">
+                    <h2 class="section-heading">Compétences numérique</h2>
+                    <hr class="light">
+                    <p id="test" class="text-faded">Start Bootstrap has everything you need to get your new website up and running in no time! All of the templates and themes on Start Bootstrap are open source, free to download, and easy to use. No strings attached!</p>
 
-    <section class="no-padding" id="competence">
-        <div class="container-fluid">
-                <div class="row">
-                    <div  class="col-lg-12 text-center">
-                        <h2 class="section-heading">Compétences Numériques</h2>
-                        <hr class="primary">
-                    </div>
                 </div>
-            </div>
-            <div class="row no-gutter popup-gallery">
-                <div class="col-lg-4 col-sm-6">
-                    <div id="chartContainer" style="height: 400px; width: 100%;">
-                	   </div>
-                </div>
-
-                <div class="col-lg-4 col-sm-6">
-                    <a href="img/portfolio/fullsize/2.jpg" class="portfolio-box">
-                        <img src="img/portfolio/thumbnails/2.jpg" class="img-responsive" alt="">
-                        <div class="portfolio-box-caption">
-                            <div class="portfolio-box-caption-content">
-                                <div class="project-category text-faded">
-                                    Category
-                                </div>
-                                <div class="project-name">
-                                    Project Name
-                                </div>
-                            </div>
-                        </div>
-                    </a>
-                </div>
-                <div class="col-lg-4 col-sm-6">
-                    <a href="img/portfolio/fullsize/3.jpg" class="portfolio-box">
-                        <img src="img/portfolio/thumbnails/3.jpg" class="img-responsive" alt="">
-                        <div class="portfolio-box-caption">
-                            <div class="portfolio-box-caption-content">
-                                <div class="project-category text-faded">
-                                    Category
-                                </div>
-                                <div class="project-name">
-                                    Project Name
-                                </div>
-                            </div>
-                        </div>
-                    </a>
-                </div>
-                <div class="col-lg-4 col-sm-6">
-                    <a href="img/portfolio/fullsize/4.jpg" class="portfolio-box">
-                        <img src="img/portfolio/thumbnails/4.jpg" class="img-responsive" alt="">
-                        <div class="portfolio-box-caption">
-                            <div class="portfolio-box-caption-content">
-                                <div class="project-category text-faded">
-                                    Category
-                                </div>
-                                <div class="project-name">
-                                    Project Name
-                                </div>
-                            </div>
-                        </div>
-                    </a>
-                </div>
-                <div class="col-lg-4 col-sm-6">
-                    <a href="img/portfolio/fullsize/5.jpg" class="portfolio-box">
-                        <img src="img/portfolio/thumbnails/5.jpg" class="img-responsive" alt="">
-                        <div class="portfolio-box-caption">
-                            <div class="portfolio-box-caption-content">
-                                <div class="project-category text-faded">
-                                    Category
-                                </div>
-                                <div class="project-name">
-                                    Project Name
-                                </div>
-                            </div>
-                        </div>
-                    </a>
-                </div>
-                <div class="col-lg-4 col-sm-6">
-                    <a href="img/portfolio/fullsize/6.jpg" class="portfolio-box">
-                        <img src="img/portfolio/thumbnails/6.jpg" class="img-responsive" alt="">
-                        <div class="portfolio-box-caption">
-                            <div class="portfolio-box-caption-content">
-                                <div class="project-category text-faded">
-                                    Category
-                                </div>
-                                <div class="project-name">
-                                    Project Name
-                                </div>
-                            </div>
-                        </div>
-                    </a>
-                </div>
-            </div>
+            </div
         </div>
     </section>
 
